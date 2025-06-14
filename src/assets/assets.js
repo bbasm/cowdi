ce1f72d8e1bab9a4200e24ac5950bd82434706fc
Basma <basmabazeid@gmail.com>
2025-06-14 19:47:28 +0700

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import crop_logo from './crop_logo.png'
+import full_logo from './full_logo.png'
+import duck from './duck.png'
+import background from './background.png'
+
+export const assets = {
+    crop_logo,
+    full_logo,
+    duck,
+    background
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { assets } from "./../assets/assets";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="bg-transparent navbarclass">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* Logo */}
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={assets.crop_logo} className="logo" alt="logo" />
+        </a>
+
+        {/* Hamburger Icon */}
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          type="button"
+          className="no-outline inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[var(--poporange)] rounded-lg md:hidden hover:bg-gray-100 focus:outline-none"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+
+        {/* Nav Items */}
+        <div
+          className={`w-full md:block md:w-auto ${
+            menuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-default"
+        >
+          {/* here to make the bg of the navbar hamburger */}
+          <ul className="nav-item-spacing font-medium flex flex-col p-4 md:p-0 mt-4 md:mt-0 border-3 border-[var(--poporange)] rounded-lg bg-FCF4EB  md:flex-row md:space-x-8 rtl:space-x-reverse md:border-0 md:bg-transparent">
+            <li>
+              <a href="#" className="sub-text py-2 px-3">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="sub-text py-2 px-3">
+                Tentang
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="sub-text code pt-10 pb-2 px-3 md:pt-2 md:pb-2 md:my-0"
+              >
+                Mulai Coding
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
