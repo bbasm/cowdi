47e17f0043d42cf8580d7ce92f0794a32f6cd914
Basma <basmabazeid@gmail.com>
2025-06-14 20:16:42 +0700

    added tentang page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,14 @@ import crop_logo from './crop_logo.png'
 import full_logo from './full_logo.png'
 import duck from './duck.png'
 import background from './background.png'
+import tentang_img from './tentang.png'
+import cursor from './cursor.png'
 
 export const assets = {
     crop_logo,
     full_logo,
     duck,
-    background
+    background,
+    tentang_img,
+    cursor,
 }
